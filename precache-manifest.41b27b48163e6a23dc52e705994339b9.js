6657531e292909aec8bcd7e012c4586f0eddc475
Sailesh Dahal <saileshbro@gmail.com>
2020-03-24 21:16:19 +0545

    Updates

---
+++ precache-manifest.41b27b48163e6a23dc52e705994339b9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fcf3f58bfada943fe45654ae354f1e83",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1e809fd162c32889ac98",
+    "url": "/static/css/main.d24a20bc.chunk.css"
+  },
+  {
+    "revision": "a2e4be9481a54b890f97",
+    "url": "/static/js/2.7aaef059.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.7aaef059.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1e809fd162c32889ac98",
+    "url": "/static/js/main.dcfb3e9b.chunk.js"
+  },
+  {
+    "revision": "4708b13b98379c58638d",
+    "url": "/static/js/runtime-main.03d56121.js"
+  }
+]);
